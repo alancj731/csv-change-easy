09417e30dd3926ddabe15e90808083232e41eaee
alancj731 <alanchenjian@gmail.com>
2026-04-03 23:09:17 -0500

    fix backend url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        // target: 'http://localhost:8000',
+        target: 'https://csv-change-easy.vercel.app',
         changeOrigin: true,
       },
     },
